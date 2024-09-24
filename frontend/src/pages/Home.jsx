ffd9d6dfab21b1806d03c0938752737583fa36f9
teshager habtie <teshie00001@gmail.com>
2024-09-24 14:40:48 -0500

    Working on home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,10 @@ import React, { useEffect, useState } from "react";
 import { Appstate } from "../App";
 import { useContext } from "react";
 import { axiosBase } from "../axiosConfig/axiosConfig.js";
-
+import PersonIcon from "@mui/icons-material/Person";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { Link } from "react-router-dom";
+import "./allstyle.css";
 function Home() {
   const { user } = useContext(Appstate);
 
@@ -26,10 +29,57 @@ function Home() {
   }, []);
   console.log(questions);
   return (
-    <>
-      <div>{user?.username}</div>
-      <div>{user?.userid}</div>
-    </>
+    <section className="container">
+      <div className="users">
+        <div>
+          <Link to="/ask" className="toask">
+            Askquestion
+          </Link>
+        </div>
+        <div className="username">Welcome: {user?.username}</div>
+      </div>
+
+      <div>
+        {/* Render your questions */}
+        {questions.length > 0 ? (
+          <>
+            {questions.map((question, i) => (
+              <Link
+                to={`/answer/${question.questionid}`}
+                className="links"
+                key={i}
+              >
+                <div key={i} className="allquestion">
+                  <div className="profile-container">
+                    <div className="profile">
+                      <PersonIcon
+                        className="person-icon"
+                        sx={{
+                          padding: 2, // Adjust padding for desired size
+                          borderRadius: "50%", // This makes the border circular
+                          color: "#012354",
+                          width: 40, // Set width and height to make it a perfect circle
+                          height: 40, // Set height equal to width
+                          border: "2px solid #012354", // Change border color to blue
+                        }}
+                      />
+
+                      {question.username}
+                    </div>
+                    <div className="title">{question.title}</div>
+                  </div>
+                  <div>
+                    <ArrowForwardIosIcon />
+                  </div>
+                </div>
+              </Link>
+            ))}
+          </>
+        ) : (
+          <p>Loading questions...</p>
+        )}
+      </div>
+    </section>
   );
 }
 
