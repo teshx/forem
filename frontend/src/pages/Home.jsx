cdb6e887b3ad855a565436b8fcf492bde14d795f
teshager habtie <teshie00001@gmail.com>
2024-09-24 07:23:05 -0500

    modefy answer api and home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Appstate } from "../App";
 import { useContext } from "react";
+import { axiosBase } from "../axiosConfig/axiosConfig.js";
 
 function Home() {
   const { user } = useContext(Appstate);
+
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      try {
+        const response = await axiosBase.get("question/all-question");
+
+        // Extract the 'questions' array from response.data
+        if (response.data && response.data.allqu) {
+          setQuestions(response.data.allqu);
+        }
+      } catch (error) {
+        console.error("Error fetching questions:", error);
+      }
+    };
+
+    fetchQuestions();
+  }, []);
+  console.log(questions);
   return (
     <>
       <div>{user?.username}</div>
