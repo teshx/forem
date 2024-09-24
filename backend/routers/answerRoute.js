259ab30e50f982fd34e0afebb39f39614cfe6ecc
teshager habtie <teshie00001@gmail.com>
2024-09-24 08:23:52 -0500

    Work on answerRoute

--- backend/routers/answerRoute.js
+++ backend/routers/answerRoute.js
@@ -1,10 +1,15 @@
 import express from "express";
 
 import { authMiddleware } from "../middleware/authMiddleware.js";
-import { answer, specificA } from "../controlers/asworecontroler.js";
+import {
+  Addanswer,
+  specificAllanswer,
+  oneanswer,
+} from "../controlers/asworecontroler.js";
 const route = express.Router();
 
-route.post("/answer", answer);
-route.get("/all-answer", specificA);
+route.post("/answer", Addanswer);
+route.get("/all-answer", specificAllanswer);
+// route.post("/oneanswer", oneanswer);
 
 export default route;
