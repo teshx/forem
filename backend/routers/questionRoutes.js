3977da871d8a9abf8fab8ffe0ec131645fd4fe96
teshager habtie <teshie00001@gmail.com>
2024-09-24 08:24:56 -0500

    Work on questionRoutes.js

--- backend/routers/questionRoutes.js
+++ backend/routers/questionRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
 
 import { authMiddleware } from "../middleware/authMiddleware.js";
-import { asking, allquestion } from "../controlers/questionControler.js";
+import {
+  asking,
+  allquestion,
+  OnlyQuestion,
+} from "../controlers/questionControler.js";
 const route = express.Router();
 
 route.post("/ask", asking);
+// route.post("/one", OnlyQuestion);
 route.get("/all-question", allquestion);
 
 export default route;
